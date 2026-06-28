eeecdd95beec12f81574999cd44b4e8d3145106d
Manuel Quiñones <manuq@endlessaccess.org>
2026-06-27 23:40:24 -0300

    diálogo de a una sílaba, un poco más

--- godot/scenes/puzzle_parar_la_oreja/interfaz_silabeador.gd
+++ godot/scenes/puzzle_parar_la_oreja/interfaz_silabeador.gd
@@ -1,6 +1,15 @@
+class_name InterfazSilabeador
 extends Node2D
 
+signal llegó(índice_sílaba: int)
+@export var cantidad_de_sílabas: int
 
 func _draw() -> void:
 	draw_circle(Vector2.ZERO, 200, Color(0.0, 0.0, 0.0, 0.486))
 	draw_arc(Vector2.ZERO, 100, -PI/8, -PI*7/8, 20, Color.RED, 5)
+
+func iniciar() -> void:
+	for i in range(cantidad_de_sílabas):
+		await get_tree().create_timer(0.5).timeout
+		llegó.emit(i)
+		
--- godot/scenes/puzzle_parar_la_oreja/puzzle_parar_la_oreja.gd
+++ godot/scenes/puzzle_parar_la_oreja/puzzle_parar_la_oreja.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var diálogo: DiálogoSilabeador
+@export var interfaz: InterfazSilabeador
 
 var silabeo_bien := "re-[b]sul[/b]-ta-que-la-[b]jua[/b]-naes-[b]táen[/b]-ga-tu-[b]zan[/b]-doal-co-ro-[b]nel[/b]-yel-i-[b]dio[/b]-ta-le-con-[b]tó[/b]-que-[b]van[/b]-aa-ta-[b]car[/b]-con-[b]o[/b]-cho-ca-[b]ño[/b]-nes-por-el-[b]flan[/b]-co-[b]nor[/b]-te"
 var silabeo_maso := "re-[b]sul[/b]-ta-que-la-[b]jua[/b]-naes-[b]táen[/b]-ga-tu-[b]zan[/b]-doal-co-ro-[b]nel[/b]-yel-i-[b]dio[/b]-ta-le-con-[b]tó[/b]-que-[b]van[/b]-aa-ta-[b]car[/b]-con-[b]o[/b]-cho-ca-[b]ño[/b]-nes-por-el-[b]flan[/b]-co-[b]nor[/b]-te"
@@ -15,6 +16,8 @@ func _ready() -> void:
 	assert(lista_sílabas_bien.size() == lista_sílabas_maso.size())
 	assert(lista_sílabas_bien.size() == lista_sílabas_maal.size())
 
+	interfaz.cantidad_de_sílabas = lista_sílabas_bien.size()
+
 	if not diálogo.is_node_ready():
 		diálogo.ready.connect(_on_diálogo_ready)
 	else:
@@ -23,10 +26,13 @@ func _ready() -> void:
 
 func _on_diálogo_ready() -> void:
 	await diálogo.iniciar()
+	interfaz.llegó.connect(_on_interfaz_llegó)
+	interfaz.iniciar()
 
-	for i in range(lista_sílabas_bien.size()):
-		var s := lista_sílabas_bien[i] as String
-		diálogo.agregar_sílaba(s, i == lista_sílabas_bien.size() - 1)
+func _on_interfaz_llegó(índice_sílaba: int) -> void:
+	var s := lista_sílabas_bien[índice_sílaba] as String
+	var última := índice_sílaba == lista_sílabas_bien.size() - 1
+	diálogo.agregar_sílaba(s, última)
+	if última:
 		await get_tree().create_timer(0.5).timeout
-
-	diálogo.mostrar_resultado(resultado)
+		diálogo.mostrar_resultado(resultado)
