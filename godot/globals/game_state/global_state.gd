fa1dc3cf8e33a51958baeb53d807336c2ab9437a
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-03 16:57:18 -0300

    atuendos

--- godot/globals/game_state/global_state.gd
+++ godot/globals/game_state/global_state.gd
@@ -4,7 +4,15 @@
 class_name GlobalState
 extends Resource
 
+signal cambió_atuendo
+
 @export var dia_actual: int = 1
+@export var atuendo_actual: Jugadora.Atuendos = Jugadora.Atuendos.VESTIDO:
+	set = _set_atuendo_actual
+
+func _set_atuendo_actual(nuevo_atuendo: Jugadora.Atuendos) -> void:
+	atuendo_actual = nuevo_atuendo
+	cambió_atuendo.emit()
 
 ## Generic game state facts. For quests, use [member QuestState.facts] instead.
 @export var facts: Dictionary[String, Variant]
