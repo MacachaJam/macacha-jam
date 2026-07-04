e5733e854187b167551632122c24d32d901106f3
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-04 09:00:51 -0300

    flechita de keney

--- godot/scenes/area_interactiva_diálogo/area_interactiva_diálogo.gd
+++ godot/scenes/area_interactiva_diálogo/area_interactiva_diálogo.gd
@@ -1,14 +1,13 @@
 class_name AreaInteractivaDiálogo
-extends Area3D
+extends AreaInteractiva
 ## Área que cuando se interactúa muestra un diálogo.
-
-signal fin_interactuar
 
 @export var diálogo: DialogueResource = preload("uid://dethlbsdqk344")
 
 func interactuar() -> void:
+	super()
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended, CONNECT_ONE_SHOT)
 	DialogueManager.show_dialogue_balloon(diálogo)
-	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
 func _on_dialogue_ended(_resource: DialogueResource):
 	fin_interactuar.emit()
--- godot/scenes/area_interactuar/area_interactuar.gd
+++ godot/scenes/area_interactuar/area_interactuar.gd
@@ -1,8 +1,11 @@
 extends Area3D
 
+@onready var flechita: Node3D = %Flechita
+
 var _area: Area3D
 
 func _ready() -> void:
+	flechita.visible = false
 	area_entered.connect(_on_area_entered)
 	area_exited.connect(_on_area_exited)
 
@@ -13,33 +16,26 @@ func _unhandled_input(event: InputEvent) -> void:
 		var jugadora: Jugadora = get_tree().get_first_node_in_group("player") as Jugadora
 		if jugadora:
 			jugadora.cambiar_detenida(true)
-		_area.connect("fin_interactuar", _on_fin_interactuar)
+		_area.connect("fin_interactuar", _on_fin_interactuar, CONNECT_ONE_SHOT)
+	flechita.visible = false
 	_area.call("interactuar")
 
 func _on_area_entered(area: Area3D) -> void:
 	if not area.has_method("interactuar"):
 		return
-	# TODO: poner flechita
-	prints("poner flechita")
+	flechita.visible = true
+	flechita.global_position = area.global_position
 	_area = area
-	pass
-	#if mostrando_diálogo:
-		#return
-	#var nodo := DialogueManager.show_dialogue_balloon(diálogo)
-	#mostrando_diálogo = true
-	#nodo.tree_exited.connect(_on_fin_diálogo)
 
 func _on_area_exited(area: Area3D) -> void:
 	if not area.has_method("interactuar"):
 		return
 	if area == _area:
 		_area = null
-	# TODO: sacar flechita
-	prints("sacar flechita")
-	pass
+	flechita.visible = false
 
 func _on_fin_interactuar() -> void:
-	prints(_on_fin_interactuar)
+	flechita.visible = true
 	var jugadora: Jugadora = get_tree().get_first_node_in_group("player") as Jugadora
 	if jugadora:
 		jugadora.cambiar_detenida(false)
