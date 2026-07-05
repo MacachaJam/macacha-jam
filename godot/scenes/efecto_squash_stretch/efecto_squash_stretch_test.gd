d42673d45679b565f91cf96e386b2b54f7ea5ffa
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-05 13:01:45 -0300

    ejemplo squash (#46)

---
+++ godot/scenes/efecto_squash_stretch/efecto_squash_stretch_test.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@onready var efecto_todo: EfectoSquashStretch = %EfectoTodo
+@onready var efecto_hojas: EfectoSquashStretch = %EfectoHojas
+
+
+#func _on_probar_todo_pressed() -> void:
+	#efecto_todo.squash_stretch()
+	#efecto_hojas.squash_stretch()
+#
+#func _on_probar_hojas_pressed() -> void:
+	#efecto_hojas.squash_stretch()
+#
+#func _on_probar_todo_mas_hojas_pressed() -> void:
+	#efecto_todo.squash_stretch()
+	#efecto_hojas.squash_stretch()
