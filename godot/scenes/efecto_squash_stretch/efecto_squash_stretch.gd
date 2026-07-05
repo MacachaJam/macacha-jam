a5386f036dad9b3ec30a88c9b1bbf6e40ea88732
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-05 12:50:58 -0300

    ejemplo squash

--- godot/scenes/efecto_squash_stretch/efecto_squash_stretch.gd
+++ godot/scenes/efecto_squash_stretch/efecto_squash_stretch.gd
@@ -13,9 +13,21 @@ extends Node
 
 @export_range(0, 1, .05, "or_more", "or_less") var factor_scale: float = 0.15
 
+# TODO: hacer que damped oscillator sea una tool
+# @export_tool_button("probar") var botón_probar: Callable = probar
+
 func squash_stretch() -> void:
 	for nodo: Node3D in nodos:
 		var spring := minimum_spring + (maximum_spring - minimum_spring) * randf()
 		var damp := minimum_damp + (maximum_damp - minimum_damp) * randf()
 		var velocity := minimum_velocity + (maximum_velocity - minimum_velocity) * randf()
 		DampedOscillator.animate(nodo, "scale", spring, damp, velocity, factor_scale)
+
+# TODO: hacer que damped oscillator sea una tool
+#func probar() -> void:
+	#squash_stretch()
+#func _notification(what: int) -> void:
+	#match what:
+		#NOTIFICATION_EDITOR_PRE_SAVE:
+			#for nodo: Node3D in nodos:
+				#nodo.scale = _escalas_iniciales(nodo)
