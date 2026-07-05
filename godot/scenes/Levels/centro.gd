e54a6de194fdd3a375159d0e31112221f1afbac5
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-05 12:21:36 -0300

    placeholder diálogo venta (#45)

--- godot/scenes/Levels/centro.gd
+++ godot/scenes/Levels/centro.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+const diálogo = preload("uid://dvyyx442twn7n")
+
 @onready var area_interactiva: AreaInteractiva = %AreaInteractiva
 
 func _ready() -> void:
@@ -12,8 +14,7 @@ func _desactivar_area() -> void:
 
 func _on_area_interactiva_inicio_interactuar() -> void:
 	# TODO: falta el minijuego, por ahora te regalo la info
-	GameState.global.hechos_del_dia.conseguiste_la_info = true
-	var d: DialogueResource = DialogueManager.create_resource_from_text("¡Ya tengo la info! [next=auto]")
-	DialogueManager.show_dialogue_balloon(d)
+	DialogueManager.show_dialogue_balloon(diálogo)
+	await DialogueManager.dialogue_ended
 	_desactivar_area()
 	area_interactiva.terminar_interacción()
