a046e1829c6ea8661759ee0a21f2e6189968ca6f
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-07 10:06:00 -0300

    overlay y autoputeada

--- godot/scenes/Levels/campamento.gd
+++ godot/scenes/Levels/campamento.gd
@@ -1,11 +1,28 @@
 extends Node3D
 
+@export_range(0, 10, 0.1, "or_more") var duración_overlay: float = 0.5
+@export_range(0, 10, 0.1, "or_more") var duración_fundido_overlay: float = 2.0
+@export var diálogo: DialogueResource = preload("uid://cf37hqituw4s6")
 
-# Called when the node enters the scene tree for the first time.
+@onready var jugadora: Jugadora = %Jugadora
+@onready var desde_el_centro: SpawnPoint = %DesdeElCentro
+@onready var overlay_dia: Control = %OverlayDia
+@onready var label_dia: Label = $UI/OverlayDia/CenterContainer/LabelDia
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	overlay_dia.hide()
+	if GameState.global.hechos_del_dia.get("te_descubrieron"):
+		DialogueManager.show_dialogue_balloon(diálogo, "mision_incompleta")
+	elif not jugadora.global_position.is_equal_approx(desde_el_centro.global_position):
+		mostrar_overlay_día()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func mostrar_overlay_día() -> void:
+		label_dia.text = "DÍA %d" % GameState.global.dia_actual
+		overlay_dia.show()
+		await get_tree().create_timer(duración_overlay).timeout
+		var tween := create_tween()
+		tween.tween_property(overlay_dia, "modulate:a", 0.0, duración_fundido_overlay)
+		await tween.finished
+		overlay_dia.hide()
