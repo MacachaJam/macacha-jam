2e3b65d8b38f7f7de25d655144d4384edac51eb0
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-07 13:15:38 -0300

    bloqueo salirse del mundo en el centro, no siendo criada

--- godot/scenes/activar_por_atuendo/activar_por_atuendo.gd
+++ godot/scenes/activar_por_atuendo/activar_por_atuendo.gd
@@ -1,8 +1,9 @@
 class_name ActivarPorAtuendo
 extends Node
 
-@export var area: Area3D
+@export var nodo: Node3D
 @export var atuendo: Jugadora.Atuendos
+@export var negado: bool
 
 func _ready() -> void:
 	_on_gamestate_cambió_atuendo()
@@ -10,5 +11,14 @@ func _ready() -> void:
 
 func _on_gamestate_cambió_atuendo() -> void:
 	var activar := atuendo == GameState.global.atuendo_actual
-	area.set_deferred("monitoring", activar)
-	area.set_deferred("monitorable", activar)
+	if negado:
+		activar = not activar
+	if nodo is Area3D:
+		var area := nodo as Area3D
+		area.set_deferred("monitoring", activar)
+		area.set_deferred("monitorable", activar)
+	elif nodo is CollisionShape3D:
+		var coli := nodo as CollisionShape3D
+		coli.disabled = not activar
+	else:
+		push_warning("no sé como activar %s" % nodo)
