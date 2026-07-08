c0834e3ff8f4bb6263ddb632ef82ccb0ee282904
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-08 18:03:27 -0300

    maiz

--- godot/scenes/minijuego_maiz/interfaz_maiz.gd
+++ godot/scenes/minijuego_maiz/interfaz_maiz.gd
@@ -1,2 +1,23 @@
 class_name InterfazMaiz
 extends Node2D
+
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
+func _ready() -> void:
+	desactivar()
+
+func desactivar() -> void:
+	visible = false
+	process_mode = Node.PROCESS_MODE_DISABLED
+
+func _physics_process(_delta: float) -> void:
+	if animation_player.is_playing():
+		return
+	if Input.is_action_pressed("move_left"):
+		animation_player.play("Ausente")
+	elif Input.is_action_pressed("move_right"):
+		animation_player.play("Presente")
+
+func iniciar() -> void:
+	visible = true
+	process_mode = Node.PROCESS_MODE_INHERIT
--- godot/scenes/minijuego_maiz/minijuego_maiz.gd
+++ godot/scenes/minijuego_maiz/minijuego_maiz.gd
@@ -8,17 +8,16 @@ signal fin_del_minijuego(éxito: bool)
 var _puntaje: float
 
 func iniciar() -> void:
-	interfaz.visible = true
+	interfaz.iniciar()
 	_puntaje = 0
-	# TODO jugar
-	prints("jugar")
 
 	var controles: ControlesPantalla = get_tree().get_first_node_in_group("controles_pantalla") as ControlesPantalla
 	if controles:
 		controles.cambiar_izq_der(true)
 
-	await get_tree().create_timer(2).timeout
+	await get_tree().create_timer(20).timeout
 
+	interfaz.desactivar()
 	terminar()
 
 
