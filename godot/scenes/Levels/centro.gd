70722e84e4fc455da50382776a58189cedcbccd3
Manuel Quiñones <manuq@endlessaccess.org>
2026-07-09 00:57:03 -0300

    mas clientes

--- godot/scenes/Levels/centro.gd
+++ godot/scenes/Levels/centro.gd
@@ -1,9 +1,16 @@
 extends Node3D
 
+signal llego_cliente
+
 const diálogo = preload("uid://dvyyx442twn7n")
+const civil = preload("uid://ei55uh7seli8")
 
 @onready var area_interactiva: AreaInteractiva = %AreaInteractiva
 @onready var player_minigame: AnimatedSprite3D = %PlayerMinigame
+@onready var spawn_clientes: Node3D = %SpawnClientes
+@onready var clientes: Node3D = %Clientes
+
+var errores: int
 
 func _ready() -> void:
 	if GameState.global.dia_actual != 1 or GameState.global.hechos_del_dia.get("conseguiste_la_info"):
@@ -12,6 +19,21 @@ func _ready() -> void:
 func _desactivar_area() -> void:
 	area_interactiva.set_deferred("monitoring", false)
 	area_interactiva.set_deferred("monitorable", false)
+
+func _obtener_un_spawn_clientes() -> Marker3D:
+	return spawn_clientes.get_children().pick_random() as Marker3D
+
+func spawn_cliente() -> void:
+	var n := civil.instantiate() as Civil
+	n.personaje = Civil.Personaje.MUJER_1
+	clientes.add_child(n)
+	n.global_position = _obtener_un_spawn_clientes().global_position
+	n.llegó.connect(_on_cliente_llegó)
+	var player: Node3D = get_tree().get_first_node_in_group("player")
+	n.ir_a(player.global_position)
+
+func _on_cliente_llegó() -> void:
+	llego_cliente.emit()
 
 func _on_area_interactiva_inicio_interactuar() -> void:
 	if GameState.global.atuendo_actual != Jugadora.Atuendos.VENDEDORA:
--- godot/scenes/civil/civil.gd
+++ godot/scenes/civil/civil.gd
@@ -1,12 +1,18 @@
 class_name Civil
 extends CharacterBody3D
 
+signal llegó
+
 enum Personaje { VARON_1, VARON_2, MUJER_1, MUJER_2 }
 
 @export var personaje: Personaje:
 	set = _set_personaje
 
 @onready var animated_sprite_3d: AnimatedSprite3D = %AnimatedSprite3D
+@onready var agent: NavigationAgent3D = %NavigationAgent3D
+
+@export_range(10, 1000, 10, "or_greater", "suffix:m/s") var velocidad_caminar: float = 50.0
+@export_range(10, 1000, 10, "or_greater", "suffix:m/s") var velocidad_detenerse: float = 50.0
 
 var sprite_frames_x_personaje: Dictionary[Personaje, SpriteFrames] = {
 	Personaje.VARON_1: preload("uid://cdyv603u07qoe"),
@@ -25,6 +31,35 @@ func _set_personaje(nuevo_personaje: Personaje) -> void:
 
 func _ready() -> void:
 	_set_personaje(personaje)
+	agent.target_reached.connect(_on_target_reached)
+	agent.navigation_finished.connect(_on_navigation_finished)
+
+func ir_a(global_pos: Vector3) -> void:
+	agent.target_position = global_pos
+
+func _on_target_reached() -> void:
+	llegó.emit()
+
+func _on_navigation_finished() -> void:
+	llegó.emit()
+
+func _physics_process(delta: float) -> void:
+	if agent.is_navigation_finished():
+		velocity.x = move_toward(velocity.x, 0, velocidad_detenerse * delta)
+		velocity.z = move_toward(velocity.z, 0, velocidad_detenerse * delta)
+	else:
+		var d := agent.get_next_path_position()
+		var dl := d - global_position
+		var direction := dl.normalized()
+
+		if direction:
+			velocity.x = direction.x * velocidad_caminar * delta
+			velocity.z = direction.z * velocidad_caminar * delta
+		else:
+			velocity.x = move_toward(velocity.x, 0, velocidad_caminar * delta)
+			velocity.z = move_toward(velocity.z, 0, velocidad_caminar * delta)
+
+	move_and_slide()
 
 func _process(_delta: float) -> void:
 	if velocity.is_zero_approx():
